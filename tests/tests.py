049c80c78217f1553bc07aea663250702b44c869
peerster <paer.berge@gmail.com>
2017-11-13 20:36:04 +0100

    Add test_get_orders

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 from mathemlib import Mathem
 import pytest
 import json
+import requests
 
 
 def test_failed_login(httpserver):
@@ -10,7 +11,8 @@ def test_failed_login(httpserver):
     test.mathem_url = httpserver.url
     assert test.mathem_url.startswith('http://127.0.0.1')
     with pytest.raises(Exception):
-        result = test.login()
+        test.login()
+
 
 def test_successful_login(httpserver):
     test = Mathem()
@@ -19,6 +21,19 @@ def test_successful_login(httpserver):
     test.mathem_url = httpserver.url
     assert test.mathem_url.startswith('http://127.0.0.1')
     assert test.login() == 'logged in'
+
+
+def test_get_orders(httpserver):
+    test = Mathem()
+    with open('test_orders.html') as f:
+        test_orders = f.read()
+    httpserver.serve_content(test_orders)
+    test.mathem_url = httpserver.url
+    test.session = requests.session()
+    result = test.get_orders(limit=3)
+    assert isinstance(result, dict)
+    assert len(result) == 3
+    assert result.get('20316076') == {}
 
 
 def test_get_info():
